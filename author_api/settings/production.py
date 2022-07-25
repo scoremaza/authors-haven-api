0299977a8c9829a1e80ab5780fc7ed108b5dac7e
Jonathan Constant <79689532+scoremaza@users.noreply.github.com>
2022-07-25 10:54:20 -0400

    Update production.py

--- author_api/settings/production.py
+++ author_api/settings/production.py
@@ -1,10 +1,6 @@
 from .base import *
 from .base import env
 
-
-from .base import *
-from .base import env
-
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["enlighten-e.com"])
